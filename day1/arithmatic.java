b7fd38221b0b25e7c5c5983342ce32e3e910cdd0
shilpa <kaboff09@gmail.com>
2022-11-08 21:49:11 +0530

    addind a line

--- day1/arithmatic.java
+++ day1/arithmatic.java
@@ -1,2 +1,16 @@
-package day1;public class arithmatic {
+package day1;
+import java.lang.*;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
+public class arithmatic {
+    public static void main(String[] args) {
+        float b,h,a;
+        System.out.println("Enter base and height");
+        Scanner sc=new Scanner(System.in);
+        b=sc.nextFloat();
+        h=sc.nextFloat();
+        a=b*h/2;
+        System.out.println("area is "+a);
+
+    }
 }
--- day2/string1.java
+++ day2/string1.java
@@ -1,2 +1,13 @@
-package day2;public class string1 {
+package day2;
+
+import java.util.Scanner;
+
+public class string1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter email id ");
+        String str=sc.next();
+        System.out.println(str.matches(.*gmail.*));
+
+    }
 }
