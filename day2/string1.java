5302021a0fa70eaf6a5fcd84dab452c5cc91b8bc
shilpa <kaboff09@gmail.com>
2022-11-09 11:27:46 +0530

    corrected the code

--- day2/string1.java
+++ day2/string1.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class string1 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter email id ");
-        String str=sc.next();
-        System.out.println(str.matches(.*gmail.*));
+        String str="shilpa@gmail.com";
+        int i=str.indexOf("@");
+        String uname=str.substring(0,i);
+        String domain=str.substring(i+1,str.length());
+        System.out.println("username: "+uname);
+        System.out.println("domain name: "+domain);
+
+        System.out.println(domain.startsWith("gmail"));
+
+
 
     }
 }
