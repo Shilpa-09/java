cab37fea61c29f4652a58454cf8f860489ca9727
shilpa <kaboff09@gmail.com>
2022-11-12 19:28:46 +0530

    String exercises

--- day2/string3.java
+++ day2/string3.java
@@ -2,7 +2,7 @@ package day2;
 
 public class string3 {
     public static void main(String[] args) {
-        String str="d kdjfkl  kjdokd ";
+        String str="d book  pen ";
         str=str.replaceAll("\\s+"," ").trim();
         System.out.println(str);
     }
--- day2/string4.java
+++ day2/string4.java
@@ -2,7 +2,7 @@ package day2;
 
 public class string4 {
     public static void main(String[] args) {
-        String str="   jf  sklj  f   mfndn mdllfjw  ";
+        String str="   jf  csk  f   rcb mi  ";
         str=str.replaceAll("\\s+"," ").trim();
         String w[]=str.split("\\s");
         System.out.println(w.length);
