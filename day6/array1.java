39abdf567d97935c8b7a00667b828488a4f07774
shilpa <kaboff09@gmail.com>
2022-11-17 21:08:09 +0530

    array

---
+++ day6/array1.java
@@ -0,0 +1,12 @@
+package day6;
+
+public class array1 {
+    public static void main(String[] args) {
+        int a[]={2,53,7,9,7,4,3,6};
+        int sum=0;
+        for (int i:a){
+            sum=sum+i;
+        }
+        System.out.println("sum is "+sum);
+    }
+}
