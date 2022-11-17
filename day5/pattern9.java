818d32cc535529f52f9336107e0aff914794286e
shilpa <kaboff09@gmail.com>
2022-11-17 21:07:23 +0530

    pattern

--- day5/pattern9.java
+++ day5/pattern9.java
@@ -5,8 +5,9 @@ public class pattern9 {
         for (int i=1;i<=5;i++){
             for (int j=1;j<=5;j++){
                 if(i<=j)
-                    System.out.print(("* "));
-                else System.out.print(" ");
+                    System.out.print(("*"));
+                else
+                    System.out.print(" ");
             }
             System.out.println("");
             }
